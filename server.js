001da40e392d759f703af6e3b6540b6887cd947d
Megan Wu <meganwu1994@gmail.com>
2019-07-03 13:18:04 -0500

    Data added

--- server.js
+++ server.js
@@ -8,6 +8,38 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
+
+
+// Data
+// =============================================================
+var reservations = [
+  {
+    routeName: "mrbean",
+    name: "Mr. Bean",
+    phoneno: 1234567890,
+    email: "bean@gmail.com",
+    uniqueId: 1
+  },
+  {
+    routeName: "ladybird",
+    name: "Lady Bird",
+    phoneno: 0987654321,
+    email: "bird@gmail.com",
+    uniqueId: 2
+  },
+  {
+    routeName: "trump",
+    name: "Trump",
+    phoneno: 1357924680,
+    email: "trump@gmail.com",
+    uniqueId: 3
+  }
+];
+
+
+
+
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -16,3 +48,5 @@ app.get("/", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+
